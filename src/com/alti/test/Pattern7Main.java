d05ce752afae99b65b1116f6aa49669adc454ea7
Kunal Satish Lawand <klawand@altimetrik.com>
2020-02-22 12:30:42 +0530

    pattern program

---
+++ src/com/alti/test/Pattern7Main.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.alti.test;
+
+import java.util.Scanner;
+
+/**
+ * @author klawand
+ *
+ */
+public class Pattern7Main {
+
+  /*
+	
+A
+BB
+CCC
+DDDD
+EEEEE
+FFFFFF
+	
+	
+	
+ */	
+	public static void main(String[] args) {
+	
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter size:");
+		int nextInt = scanner.nextInt();
+		//as capital letter ascii start with '65'
+		for (int i = 65; i < nextInt+65; i++) {
+			
+			for(int j=65;j<i+1;j++) {
+				System.out.print((char)i);
+			}
+			System.out.println();
+			
+			
+		}
+
+		scanner.close();
+
+	}
+		
+
+	}
+
+
